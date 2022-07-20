d77b74b951fb6433d43300a9fa0f003ae02ca76f
Sardorbek Imomaliev <imomaliev@hey.com>
2022-07-20 23:24:01 +0700

    add javascript to editorconfig and pre-commit
    - switch prettier config file to .prettierrc.js
    - use 2 space indentation for json, css and javascript file types

---
+++ .prettierrc.js
@@ -0,0 +1,47 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  overrides: [
+    {
+      files: ['*.yaml', '*.yml'],
+      options: {
+        tabWidth: 4,
+        printWidth: 88,
+      },
+    },
+    {
+      files: ['*.md'],
+      options: {
+        // There is currently a bug that affects text alignment after the bullet
+        // points in markdown
+        // https://github.com/prettier/prettier/issues/5019
+        tabWidth: 4,
+      },
+    },
+    // https://github.com/NiklasPor/prettier-plugin-go-template
+    {
+      files: ['blog/src/layouts/**/*.html'],
+      options: {
+        parser: 'go-template',
+        tabWidth: 4,
+        printWidth: 88,
+      },
+    },
+    {
+      files: ['*.json', '*.css'],
+      options: {
+        tabWidth: 2,
+        printWidth: 88,
+      },
+    },
+    {
+      files: ['*.js'],
+      options: {
+        tabWidth: 2,
+        printWidth: 88,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    },
+  ],
+}
--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/layouts/**/*.{html,js}"],
+  content: ['./src/layouts/**/*.{html,js}'],
   theme: {
     extend: {},
   },
